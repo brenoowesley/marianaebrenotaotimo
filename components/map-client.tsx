5dffa23f6300660898b9fd2ecd2136b12dd17471
brenoowesley <breno_wesley@yahoo.com.br>
2025-11-23 21:05:26 -0300

    Update map-client.tsx

--- components/map-client.tsx
+++ components/map-client.tsx
@@ -22,50 +22,52 @@ interface MapLocation {
 interface MapClientProps {
     locations: MapLocation[]
 }
-// Center on Natal/RN, Brazil
-const center: LatLngExpression = [-5.79448, -35.211]
-const defaultZoom = locations.length > 0 ? 13 : 12
 
-return (
-    <MapContainer
-        center={center}
-        zoom={defaultZoom}
-        style={{ height: '100%', width: '100%' }}
-        className="z-0"
-    >
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+export default function MapClient({ locations }: MapClientProps) {
+    // Center on Natal/RN, Brazil
+    const center: LatLngExpression = [-5.79448, -35.211]
+    const defaultZoom = locations.length > 0 ? 13 : 12
 
-        <MapOverlay locations={locations} />
+    return (
+        <MapContainer
+            center={center}
+            zoom={defaultZoom}
+            style={{ height: '100%', width: '100%' }}
+            className="z-0"
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
 
-        {locations.map((location) => (
-            <CircleMarker
-                key={location.id}
-                center={[location.lat, location.lng]}
-                radius={8}
-                pathOptions={{
-                    fillColor: '#3b82f6',
-                    fillOpacity: 0.8,
-                    color: '#1e40af',
-                    weight: 2,
-                }}
-            >
-                <Popup>
-                    <div dangerouslySetInnerHTML={{
-                        __html: renderToStaticMarkup(
-                            <MapPopup
-                                title={location.title}
-                                coverImage={location.coverImage || null}
-                                notes={location.notes}
-                                rating={location.rating || null}
-                            />
-                        )
-                    }} />
-                </Popup>
-            </CircleMarker>
-        ))}
-    </MapContainer>
-)
+            <MapOverlay locations={locations} />
+
+            {locations.map((location) => (
+                <CircleMarker
+                    key={location.id}
+                    center={[location.lat, location.lng]}
+                    radius={8}
+                    pathOptions={{
+                        fillColor: '#3b82f6',
+                        fillOpacity: 0.8,
+                        color: '#1e40af',
+                        weight: 2,
+                    }}
+                >
+                    <Popup>
+                        <div dangerouslySetInnerHTML={{
+                            __html: renderToStaticMarkup(
+                                <MapPopup
+                                    title={location.title}
+                                    coverImage={location.coverImage || null}
+                                    notes={location.notes}
+                                    rating={location.rating || null}
+                                />
+                            )
+                        }} />
+                    </Popup>
+                </CircleMarker>
+            ))}
+        </MapContainer>
+    )
 }
